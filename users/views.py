6a276e0604325ca6bfa14f5d2f552e10acfe713e
saifmathur <saifmathur41@gmail.com>
2020-11-11 09:49:04 +0530

    alerts in reg form

--- users/views.py
+++ users/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+# types of messages
+# messages.info
+# messages.success
+# messages.warning
+# messages.error
 
 # Create your views here.
 
 #gonna use predefined forms
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username') #its a dictionary
+            messages.success(request, f'Account created for {username}!')
+            #gonna redirect the user back to the home page
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
-    
+
