83f4fbe3d386b6043a68b5f5e6f46c84c9aa8fd5
saifmathur <saifmathur41@gmail.com>
2020-11-12 17:15:59 +0530

    added log in and logout

--- users/views.py
+++ users/views.py
@@ -7,6 +7,20 @@ from django.contrib import messages
 #importing the changed form from forms.py which we created
 from .forms import UserRegisterForm
 
+
+'''
+after this, installed crispy forms and go to installed apps in the project folder
+and add this 'crispy_forms' in the list
+
+also you have to tell crispy_forms which css framework you want to use
+the default is bootstrap2 and we need 4
+so add this line to the bottom of the page
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+'''
+
+
+
 # types of messages
 # messages.info
 # messages.success
@@ -23,9 +37,9 @@ def register(request):
             print('FORM VALID')
             form.save() #saving the user
             username = form.cleaned_data.get('username') #its a dictionary
-            messages.success(request, f'Account created for {username}!')
-            #gonna redirect the user back to the home page
-            return redirect('blog-home')
+            messages.success(request, f'Account created for {username}! You can now Login.')
+            #gonna redirect the user to the log in page
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
