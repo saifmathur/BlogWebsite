716ce0194b6a0c2ad3913f394f1441655c3ef128
saifmathur <saifmathur41@gmail.com>
2021-01-18 20:58:03 +0530

    added PostListView for class based views

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from . views import PostListView
+
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    #path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
+
+# when we use class based views we need to convert the class into an actual view by using the 'as_view()' method
+# now this change will give
