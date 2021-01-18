52f391b19d2fd0a2704d4f9f757238b579a8b1b0
saifmathur <saifmathur41@gmail.com>
2021-01-18 20:47:10 +0530

    Fresh

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 # from django.http import HttpResponse
 from .models import Post #importing the posts that we created 
 
@@ -37,6 +38,24 @@ def home(request):
         'title' : 'Home',
     }
     return render(request, 'blog/home.html', context)
+
+
+
+
+class PostListView(ListView):
+    model = Post
+
+
+
+
+
+
+
+
+
+
+
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
